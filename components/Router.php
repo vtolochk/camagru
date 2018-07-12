75fe4318a11a1b442dbc45a1f74c12f5ff09788d
Valentin Tolochko <vtolochk@gmail.com>
2018-07-12 13:24:41 +0300

    second commit

---
+++ components/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+class Router
+{
+    private $routes;
+
+    public function __construct()
+    {
+        $routesPath = ROOT.'/config/routes.php';
+        $this->routes = include($routesPath);
+    }
+    public function run()
+    {
+        
+    }
+}
--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Router.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    fdsfspdgpsopkoo
-</body>
-</html>
+
+$router = new Router();
+$router->run();
