44bc4bbba399e1a9828f70a18a393c40948387e7
Valentin Tolochko <vtolochk@gmail.com>
2018-07-12 13:46:26 +0300

    error managment in browser

--- components/Router.php
+++ components/Router.php
@@ -9,8 +9,18 @@ class Router
         $routesPath = ROOT.'/config/routes.php';
         $this->routes = include($routesPath);
     }
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
     public function run()
     {
-        
+        $uri = $this->getURI();
+        foreach ($this->routes as $uriPattern => $path)
+        {
+            echo $path . "\n";
+        }
     }
 }
