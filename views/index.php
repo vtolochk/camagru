1e5c3b0a8aee12fe937555a0900786cb12a8005d
Valentin Tolochko <vtolochk@gmail.com>
2018-07-20 16:00:21 +0300

    main page almoste done

--- views/index.php
+++ views/index.php
@@ -2,23 +2,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Camagru</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
     <link href="/views/css/style.css" rel="stylesheet">
 </head>
 <body>
-        <div class="container">
-            <div class="header">
-                <p>Welcome to camagru web app! Its a simple web apllication for creating photos with stickers.</p>
-                <div class="navigation">
-                 kaka
+    <div class="Hcontainer">
+            <div class="description">
+                <h2>Welcome to camagru web app! It is a simple web apllication for creating photos with stickers.</h2>
+            </div>
+            <div class="buttonsOrInfo">
+                <div class="signin">
+                    <button id="signIn">Sign in</button>
+                </div>
+                <div class="signup">
+                    <button id="signUp">Sign up</button>
                 </div>
             </div>
-            <div class="main">
+            <div class="Photos">
+                <div class="main1">
                     <p>Some photos will be here soon</p>
-            </div>
-            <div class="footer">
-
-            </div>
+                </div>
+                <div class="main2">
+                    <p>Some photos will be here soon</p>
+                </div>
+                <div class="main3">
+                    <p>Some photos will be here soon</p>
+                </div>
+        </div>
         </div>
 </body>
 </html>
